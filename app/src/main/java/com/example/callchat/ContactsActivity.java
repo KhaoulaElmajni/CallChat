1a5fdaec47c9b0a4ec7b1447a83eeebe7688e7e2
KhaoulaElmajni <50221822+KhaoulaElmajni@users.noreply.github.com>
2020-03-13 01:41:17 +0100

    video call

--- app/src/main/java/com/example/callchat/ContactsActivity.java
+++ app/src/main/java/com/example/callchat/ContactsActivity.java
@@ -2,12 +2,25 @@ package com.example.callchat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,13 +31,25 @@ public class ContactsActivity extends AppCompatActivity {
     BottomNavigationView navView;
     RecyclerView myContactsList ;
     ImageView findPeopleBtn;
+    private DatabaseReference  contactsRef,usersRef;
+    private FirebaseAuth mAuth;
+    private String currentUserId;
+    private String userName="", profileImage="", calledBy="";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
-         navView = findViewById(R.id.nav_view);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        contactsRef = FirebaseDatabase.getInstance().getReference().child("Contacts");
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+
+        navView = findViewById(R.id.nav_view);
          navView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
          findPeopleBtn = findViewById(R.id.find_people_btn);
          myContactsList = findViewById(R.id.contact_list);
@@ -70,7 +95,134 @@ public class ContactsActivity extends AppCompatActivity {
     };
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
 
+        checkForReceivingCall();
+
+        validateUser();
+
+
+
+        FirebaseRecyclerOptions<Contacts>options
+                = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(contactsRef.child(currentUserId),Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts,ContactsViewHolder> firebaseRecyclerAdapter
+                =new FirebaseRecyclerAdapter<Contacts, ContactsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final ContactsViewHolder holder, int i, @NonNull Contacts contacts) {
+
+                final String listUserId = getRef(i).getKey();
+
+
+                usersRef.child(listUserId).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists())
+                        {
+                            userName=dataSnapshot.child("name").getValue().toString();
+                            profileImage=dataSnapshot.child("image").getValue().toString();
+
+                            holder.userNameTxt.setText(userName);
+                            Picasso.get().load(profileImage).into(holder.profileImageView);
+
+                        }
+                        holder.calltBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent callingIntent = new Intent(ContactsActivity.this, CallingActivity.class);
+                                callingIntent.putExtra("visit_user_id",listUserId);
+                                startActivity(callingIntent);
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_design, parent,false);
+                ContactsViewHolder viewHolder = new ContactsViewHolder(view);
+                return viewHolder;
+            }
+        };
+        myContactsList.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
+    }
+
+
+
+
+    public static class ContactsViewHolder extends RecyclerView.ViewHolder
+    {
+
+        TextView userNameTxt;
+        Button calltBtn ;
+        ImageView profileImageView ;
+
+
+
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userNameTxt = itemView.findViewById(R.id.name_contact);
+            calltBtn = itemView.findViewById(R.id.call_btn);
+            profileImageView = itemView.findViewById(R.id.image_contact);
+
+
+        }
+    }
+
+    private  void validateUser()
+    {
+DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+reference.child("Users").child(currentUserId).addValueEventListener(new ValueEventListener() {
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        if (!dataSnapshot.exists())
+        {
+            Intent settingsIntent = new Intent(ContactsActivity.this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            finish();
+        }
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+
+    }
+});
+
+    }
+    private void checkForReceivingCall() {
+    usersRef.child(currentUserId).child("Ringing")
+            .addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.hasChild("ringing")){
+                        calledBy = dataSnapshot.child("ringing").getValue().toString();
+
+                        Intent callingIntent = new Intent(ContactsActivity.this, CallingActivity.class);
+                        callingIntent.putExtra("visit_user_id",calledBy);
+                        startActivity(callingIntent);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+    }
 
 }
---
+++ app/src/main/java/com/example/callchat/VideoChatActivity.java
@@ -0,0 +1,242 @@
+package com.example.callchat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+
+import android.service.voice.VoiceInteractionSession;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.opentok.android.OpentokError;
+import com.opentok.android.Publisher;
+import com.opentok.android.PublisherKit;
+import com.opentok.android.Session;
+import com.opentok.android.Session.Builder;
+import com.opentok.android.Stream;
+import com.opentok.android.Subscriber;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.se.omapi.Session.*;
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
+public class VideoChatActivity extends AppCompatActivity
+        implements Session.SessionListener, PublisherKit.PublisherListener {
+
+    private static String API_Key = "46539322";
+    private static String SESSION_ID = "2_MX40NjUzOTMyMn5-MTU4NDA1NzY0NjM4Mn5LaGNud2hBUUVjaUhlZXRpa0RFRWVOS1V-fg";
+    private static String TOKEN = "T1==cGFydG5lcl9pZD00NjUzOTMyMiZzaWc9MTZlOTQyYjQ0ODdhOWU5N2FkYjNiNDEyYWExNDBlMjBlZTNiZDc0YjpzZXNzaW9uX2lkPTJfTVg0ME5qVXpPVE15TW41LU1UVTROREExTnpZME5qTTRNbjVMYUdOdWQyaEJVVVZqYVVobFpYUnBhMFJGUldWT1MxVi1mZyZjcmVhdGVfdGltZT0xNTg0MDU3NzU5Jm5vbmNlPTAuMTM4Mjc0NTg3MDA1Mjc3OTUmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU4NjY0OTc1OCZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==";
+    private static final String LOG_TAG = VideoChatActivity.class.getSimpleName();
+    private static final int RC_VIDEO_APP_PERM = 124;
+    private ImageView closeVideoChatBtn ;
+    private DatabaseReference usersRef ;
+    private String userID ="";
+    private FrameLayout mPublisherViewController;
+    private FrameLayout mSubscriberViewController;
+    private Session mSession;
+    private Publisher mPublisher;
+    private Subscriber mSubscriber;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat);
+
+        userID = FirebaseAuth .getInstance().getCurrentUser().getUid();
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        closeVideoChatBtn = findViewById(R.id.close_video_chat_btn);
+        closeVideoChatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usersRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(userID).hasChild("Ringing"))
+                        {
+                            usersRef.child(userID).child("Ringing").removeValue();
+
+                            if (mPublisher != null)
+                            {
+                                mPublisher.destroy();
+                            }
+                            if (mSubscriber != null)
+                            {
+                                mSubscriber.destroy();
+                            }
+
+                            startActivity(new Intent(VideoChatActivity.this,RegistreActivity.class));
+                            finish();
+                        }
+                        if (dataSnapshot.child(userID).hasChild("Ringing"))
+                        {
+                            usersRef.child(userID).child("Calling").removeValue();
+
+                            if (mPublisher != null)
+                            {
+                                mPublisher.destroy();
+                            }
+                            if (mSubscriber != null)
+                            {
+                                mSubscriber.destroy();
+                            }
+
+                            startActivity(new Intent(VideoChatActivity.this,RegistreActivity.class));
+                            finish();
+                        }
+                        else
+                        {
+                            if (mPublisher != null)
+                            {
+                                mPublisher.destroy();
+                            }
+                            if (mSubscriber != null)
+                            {
+                                mSubscriber.destroy();
+                            }
+
+                            startActivity(new Intent(VideoChatActivity.this,RegistreActivity.class));
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+        });
+
+        requestPermission();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,VideoChatActivity.this);
+
+    }
+    @AfterPermissionGranted(RC_VIDEO_APP_PERM)
+    private void requestPermission()
+    {
+        String[]perms = {Manifest.permission.INTERNET,Manifest.permission.CAMERA,Manifest.permission.RECORD_AUDIO};
+
+
+        if (EasyPermissions.hasPermissions(this,perms))
+        {
+            mPublisherViewController = findViewById(R.id.publisher_container);
+            mSubscriberViewController = findViewById(R.id.subscriber_container);
+
+            //1.initialize and connect to the session
+           mSession = new Session.Builder(this,API_Key,SESSION_ID).build();
+           mSession.setSessionListener(VideoChatActivity.this);
+
+           mSession.connect(TOKEN);
+        }
+        else
+        {
+            EasyPermissions.requestPermissions(this,"Hi this app needs the Mic and Camera permissions, Please allow",RC_VIDEO_APP_PERM,perms);
+
+        }
+    }
+
+
+
+
+    @Override
+    public void onStreamCreated(PublisherKit publisherKit, Stream stream) {
+
+    }
+
+    @Override
+    public void onStreamDestroyed(PublisherKit publisherKit, Stream stream) {
+
+    }
+
+    @Override
+    public void onError(PublisherKit publisherKit, OpentokError opentokError) {
+
+    }
+
+    //2.Publishing a stream to  the session
+    @Override
+    public void onConnected(Session session) {
+        Log.i(LOG_TAG,"Session Connected");
+
+        mPublisher = new Publisher.Builder(this).build();
+        mPublisher.setPublisherListener(VideoChatActivity.this);
+
+        mPublisherViewController.addView(mPublisher.getView());
+
+        if (mPublisher.getView() instanceof GLSurfaceView)
+        {
+            ((GLSurfaceView)mPublisher.getView()).setZOrderOnTop(true);
+        }
+        mSession.publish(mPublisher);
+
+    }
+
+    @Override
+    public void onDisconnected(Session session) {
+        Log.i(LOG_TAG,"Stream Disconnecteded");
+
+    }
+
+
+    //3. Subscribing to the streams
+    @Override
+    public void onStreamReceived(Session session, Stream stream) {
+        Log.i(LOG_TAG,"Stream Received");
+        if (mSubscriber == null)
+        {
+            mSubscriber = new Subscriber.Builder(this,stream).build();
+            mSession.subscribe(mSubscriber);
+            mSubscriberViewController.addView(mSubscriber.getView());
+        }
+    }
+
+    @Override
+    public void onStreamDropped(Session session, Stream stream) {
+        Log.i(LOG_TAG,"Stream Dropped");
+        if (mSubscriber!= null)
+        {
+            mSubscriber = null;
+            mSubscriberViewController.removeAllViews();
+        }
+
+    }
+
+    @Override
+    public void onError(Session session, OpentokError opentokError) {
+        Log.i(LOG_TAG,"Stream Error");
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
